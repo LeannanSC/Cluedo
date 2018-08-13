a293f4c964e809edc649f0bfe7a35182ef847c39
Sophie <sophie.cotterill@gmail.com>
2018-08-13 14:34:33 +1200

    Initialise tiles and player starting location. Draw methods need to be tested

--- src/main/java/RoomTile.java
+++ src/main/java/RoomTile.java
@@ -1,7 +1,11 @@
 public class RoomTile extends Tile {
-    //todo ask sophie about room representation
+
+	private boolean hasEntry;
+
+    // TODO: ask sophie about room representation
     public RoomTile(String name, String[] drawMethod) {
         super(name, true, drawMethod);
     }
+
 
 }
